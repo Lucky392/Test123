2165a1023ecc74c295095152eed4d21ec08f7412
Lucky392 <lazar_92@live.com>
2015-08-31 14:53:25 +0200

    first commit

--- src/main/java/rs/htec/cms/cms_bulima/service/UserCmsRESTEndpoint.java
+++ src/main/java/rs/htec/cms/cms_bulima/service/UserCmsRESTEndpoint.java
@@ -5,14 +5,10 @@
  */
 package rs.htec.cms.cms_bulima.service;
 
-import java.sql.Timestamp;
 import java.util.Base64;
-import java.util.List;
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
