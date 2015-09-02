25b896000418ea7abd678a6277b64dc7a6fdcb5e
Lucky392 <lazar_92@live.com>
2015-09-02 16:02:01 +0200

    nice

--- src/main/java/rs/htec/cms/cms_bulima/service/NewsCmsRESTEndpoint.java
+++ src/main/java/rs/htec/cms/cms_bulima/service/NewsCmsRESTEndpoint.java
@@ -9,8 +9,10 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.PUT;
@@ -37,7 +39,7 @@ public class NewsCmsRESTEndpoint {
     }
 
     @GET
-    @Path("/{page}/{limit}")
+    @Path("/{page}/{limit}/")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getNews(@HeaderParam("authorization") String token, @PathParam("page") int page, @PathParam("limit") int limit) {
         EntityManager em = helper.getEntityManager();
@@ -55,14 +57,43 @@ public class NewsCmsRESTEndpoint {
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    @Path("/insert")
-    public Response insertNews(News news) {
+    @Path("/")
+    public Response insertNews(@HeaderParam("authorization") String token, News news) {
         EntityManager em = helper.getEntityManager();
-        news.setCreateDate(new Date());
-        em.getTransaction().begin();
-        em.persist(news);
-
-        em.getTransaction().commit();
-        return Response.ok("blaaa").build();
+        try {
+            CmsUser user = em.find(CmsUser.class, Long.parseLong(helper.decode(token).split("##")[1]));
+            if (user.getToken() != null && !user.getToken().equals("")) {
+                news.setCreateDate(new Date());
+                em.getTransaction().begin();
+                em.persist(news);
+                em.getTransaction().commit();
+                return Response.ok().build();
+            }
+        } catch (Exception ignore) {
+        }
+        return Response.status(Response.Status.UNAUTHORIZED).build();
     }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteNews(@HeaderParam("authorization") String token, @PathParam("id") long id) {
+        EntityManager em = helper.getEntityManager();
+        try {
+            CmsUser user = em.find(CmsUser.class, Long.parseLong(helper.decode(token).split("##")[1]));
+            if (user.getToken() != null && !user.getToken().equals("")) {
+                News news = em.find(News.class, id);
+                if (news != null) {
+                    em.getTransaction().begin();
+                    em.remove(news);
+                    em.getTransaction().commit();
+                    return Response.ok().build();
+                } else {
+                    return Response.status(Response.Status.BAD_REQUEST).build();
+                }
+            }
+        } catch (Exception ignore) {
+        }
+        return Response.status(Response.Status.UNAUTHORIZED).build();
+    }
+
 }
