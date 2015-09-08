67b401ec23a115577999e7aa35987e877ece1201
Lucky392 <lazar_92@live.com>
2015-09-08 10:03:56 +0200

    added generic exceptions

--- src/main/java/rs/htec/cms/cms_bulima/service/SliderContentCmsRESTEndpoint.java
+++ src/main/java/rs/htec/cms/cms_bulima/service/SliderContentCmsRESTEndpoint.java
@@ -26,7 +26,6 @@ import rs.htec.cms.cms_bulima.constants.TableConstants;
 import rs.htec.cms.cms_bulima.domain.CmsUser;
 import rs.htec.cms.cms_bulima.domain.SliderContent;
 import rs.htec.cms.cms_bulima.exception.DataNotFoundException;
-import rs.htec.cms.cms_bulima.exception.ForbbidenException;
 import rs.htec.cms.cms_bulima.exception.NotAuthorizedException;
 import rs.htec.cms.cms_bulima.helper.RestHelperClass;
 import rs.htec.cms.cms_bulima.token.AbstractTokenCreator;
@@ -66,22 +65,15 @@ public class SliderContentCmsRESTEndpoint {
     public Response insertSlider(@HeaderParam("authorization") String token, SliderContent slider) {
         EntityManager em = helper.getEntityManager();
         try {
-            CmsUser user = em.find(CmsUser.class, Long.parseLong(tokenHelper.decode(token).split("##")[1]));
-            if (user.getToken() != null && !user.getToken().equals("")) {
-                if (helper.isAdmin(user)) {
-                    slider.setCreateDate(new Date());
-                    em.getTransaction().begin();
-                    em.persist(slider);
-                    em.getTransaction().commit();
-                    return Response.status(Response.Status.CREATED).build();
-                } else {
-                    throw new ForbbidenException("You don't have permission to insert data");
-                }
-            } else {
-                throw new NotAuthorizedException("You are not logged in!");
-            }
+            helper.checkUserAndPrivileges(em, TableConstants.NEWS, MethodConstants.ADD, token);
+            slider.setCreateDate(new Date());
+            em.getTransaction().begin();
+            em.persist(slider);
+            em.getTransaction().commit();
+            return Response.status(Response.Status.CREATED).build();
         } catch (IllegalArgumentException ex) {
-            Logger.getLogger(NewsCmsRESTEndpoint.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(NewsCmsRESTEndpoint.class
+                    .getName()).log(Level.SEVERE, null, ex);
             throw new NotAuthorizedException("You are not logged in!");
         }
     }
@@ -90,27 +82,21 @@ public class SliderContentCmsRESTEndpoint {
     @Path("/{id}")
     public Response deleteSlider(@HeaderParam("authorization") String token, @PathParam("id") long id) {
         EntityManager em = helper.getEntityManager();
+
         try {
-            CmsUser user = em.find(CmsUser.class, Long.parseLong(tokenHelper.decode(token).split("##")[1]));
-            if (user.getToken() != null && !user.getToken().equals("")) {
-                if (helper.isAdmin(user)) {
-                    SliderContent slider = em.find(SliderContent.class, id);
-                    if (slider != null) {
-                        em.getTransaction().begin();
-                        em.remove(slider);
-                        em.getTransaction().commit();
-                        return Response.ok().build();
-                    } else {
-                        throw new DataNotFoundException("Slider at index: " + id + " does not exits");
-                    }
-                } else {
-                    throw new ForbbidenException("You don't have permission to delete data");
-                }
+            helper.checkUserAndPrivileges(em, TableConstants.NEWS, MethodConstants.DELETE, token);
+            SliderContent slider = em.find(SliderContent.class, id);
+            if (slider != null) {
+                em.getTransaction().begin();
+                em.remove(slider);
+                em.getTransaction().commit();
+                return Response.ok().build();
             } else {
-                throw new NotAuthorizedException("You are not logged in!");
+                throw new DataNotFoundException("Slider at index: " + id + " does not exits");
             }
         } catch (IllegalArgumentException ex) {
-            Logger.getLogger(NewsCmsRESTEndpoint.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(NewsCmsRESTEndpoint.class
+                    .getName()).log(Level.SEVERE, null, ex);
             throw new NotAuthorizedException("You are not logged in!");
         }
     }
@@ -122,26 +108,20 @@ public class SliderContentCmsRESTEndpoint {
         EntityManager em = helper.getEntityManager();
         try {
             CmsUser user = em.find(CmsUser.class, Long.parseLong(tokenHelper.decode(token).split("##")[1]));
-            if (user.getToken() != null && !user.getToken().equals("")) {
-                if (helper.isAdmin(user)) {
-                    SliderContent oldSlider = em.find(SliderContent.class, slider.getId());
-                    if (oldSlider != null) {
-                        slider.setCreateDate(new Date());
-                        em.getTransaction().begin();
-                        em.merge(slider);
-                        em.getTransaction().commit();
-                        return Response.ok("Successfully updated!").build();
-                    } else {
-                        throw new DataNotFoundException("Slider at index" + slider.getId() + " does not exits");
-                    }
-                } else {
-                    throw new ForbbidenException("You don't have permission to update data");
-                }
+            helper.checkUserAndPrivileges(em, TableConstants.NEWS, MethodConstants.EDIT, token);
+            SliderContent oldSlider = em.find(SliderContent.class, slider.getId());
+            if (oldSlider != null) {
+                slider.setCreateDate(new Date());
+                em.getTransaction().begin();
+                em.merge(slider);
+                em.getTransaction().commit();
+                return Response.ok("Successfully updated!").build();
             } else {
-                throw new NotAuthorizedException("You are not logged in!");
+                throw new DataNotFoundException("Slider at index" + slider.getId() + " does not exits");
             }
         } catch (IllegalArgumentException ex) {
-            Logger.getLogger(NewsCmsRESTEndpoint.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(NewsCmsRESTEndpoint.class
+                    .getName()).log(Level.SEVERE, null, ex);
             throw new NotAuthorizedException("You are not logged in!");
         }
     }
