3f8d20e94fccbc0788acbf3af5c60bcb170b4578
Stefan Stanojevic <mrstefan1100@gmail.com>
2015-09-22 12:18:05 +0200

    Sve

--- src/main/java/rs/htec/cms/cms_bulima/service/StatisticRESTEndPoint.java
+++ src/main/java/rs/htec/cms/cms_bulima/service/StatisticRESTEndPoint.java
@@ -40,48 +40,6 @@ public class StatisticRESTEndPoint {
     }
 
     /**
-     * API for method: /manager/{email}<br>
-     * This method returns JSON list of Fantasy manager.
-     * It produces APPLICATION_JSON media type. Example for JSON list: <br/>
-     * [{<br/>
-     * "idFantasyClub": "null",<br/>
-     * "newsHeadlineMobile": "NEUER TRANSFER",<br/>
-     * "newsHeadlineWeb": "NEUES VOM TRANSFERMARKT",<br/>
-     * "newsMessageWeb": "Kehrer wechselt für 100.000 von Los Chipirones zu
-     * Sport1",<br/>
-     * "newsMessageMobile": "Kehrer wechselt für 100.000 von Los Chipirones zu
-     * Sport1",<br/>
-     * "id": "4",<br/>
-     * "newsDate": "2015-07-20 15:32:35.0",<br/>
-     * "newsType": "transfer",<br/>
-     * "createDate": "2015-07-20 15:32:36.0",<br/>
-     * "idFantasyLeague": "rs.htec.cms.cms_bulima.domain.FantasyLeague[ id=7175
-     * ]"<br/>
-     * },<br/>
-     * {<br/>
-     * "idFantasyClub": "rs.htec.cms.cms_bulima.domain.FantasyClub[ id=27483
-     * ]",<br/>
-     * "newsHeadlineMobile": "NEUE AUFSTELLUNG",<br/>
-     * "newsHeadlineWeb": "NEUE AUFSTELLUNG",<br/>
-     * "newsMessageWeb": "System: 4-3-3{@code <br/>}Tor:
-     * Langerak{@code <br/>}Abwehr: Garcia, Schär, Matip,
-     * Felipe{@code <br/>}Mittelfeld: Clemens, Holtby,
-     * Schwegler{@code <br/>}Sturm: Kurányi, Kachunga, Osako",
-     * "newsMessageMobile": "Für den kommenden 2. Spieltag hast du ein
-     * 4-3-3-System mit folgenden Spielern aufgestellt:{@code <br/>}Tor:
-     * Langerak{@code <br/>}Abwehr: Garcia, Schär, Matip,
-     * Felipe{@code <br/>}Mittelfeld: Clemens, Holtby,
-     * Schwegler{@code <br/>}Sturm: Kurányi, Kachunga, Osako",<br/> "id":
-     * "5",<br/> "newsDate": "2015-08-17 14:47:54.0",<br/>
-     * "newsType": "lineup",<br/> "createDate": "2015-07-20 15:33:48.0",<br/>
-     * "idFantasyLeague": "null"<br/> } ] You can also
-     *
-     * @param token - header authorization
-     * @param email - user email
-     * @return {@link Response} Response {@link Status#OK} 200 OK with JSON body
-     * @throws DataNotFoundException
-     * @throws NotAuthorizedException
-     *
      * This method returns JSON list of Fantasy Managers for one user. It
      * produces APPLICATION_JSON media type. Example for JSON list: <br/>[
      * {<br/>
